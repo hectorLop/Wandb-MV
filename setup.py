c89bd5eb32e4a33e2c31f09aa60922ee35f5505f
hectorLop <lopez.almazan.hector@gmail.com>
2022-02-04 14:30:45 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='wandb_mv',
-    version='0.1.0',    
+    name='wandb-mv',
+    version='0.2.4',    
     description='Model Versioning using Weight & Biases',
     url='https://github.com/hectorLop/Wandb-MV',
     author='Hector Lopez',
     author_email='lopez.almazan.hector@gmail.com',
     license='MIT',
+    py_modules=['wandb_mv'],
+    packages=['wandb_mv'],
     install_requires=['wandb']
 )
